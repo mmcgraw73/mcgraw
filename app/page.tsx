8a2b756ee4e90e48ddb03b3459436346b41639d8
Michael McGraw <michael@mcgraw.codes>
2024-07-09 11:44:27 -0400

    min update to home page

--- app/page.tsx
+++ app/page.tsx
@@ -8,7 +8,7 @@ export default function Home() {
     <main className="flex min-h-screen min-w-screen flex-col overflow-hidden">
     <Vanta />
     <div className="flex flex-col p-10">
-      <span className="text-3xl">m.mcgraw</span>
+      <span className="text-3xl border-b w-1/4">m.mcgraw</span>
       <span className="pt-2 w-1/4">a frontend engineer with an eye for design and decade of experience in web development and ui technologies</span>
       <span className="text-sm text-orange-500 self-end">this portfolio is currently being built with Next.js & TypeScript</span>
       <span className="text-sm text-orange-500 self-end">check back soon for more updates</span>

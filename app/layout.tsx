2bceb24cebbe2bf327b3c2f9866f69c289b51235
Michael McGraw <michael@mcgraw.codes>
2024-07-10 15:23:14 -0400

    sytax error in file name / take 2

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,10 +1,8 @@
-import type { Metadata } from "next";
-import { Inter } from "next/font/google";
+
 import "./globals.css";
 import Nav from "./components/nav";
 import { Analytics } from "@vercel/analytics/react";
 
-const inter = Inter({ subsets: ["latin"] });
 
 export default function RootLayout({
   children,
--- pages/more.tsx
+++ pages/more.tsx
@@ -2,7 +2,7 @@
 
 import React from 'react';
 import Vanta from "../app/components/vantabg";
-import Nav from "../app/components/nav";
+import Nav from "../app/components/Nav";
 
 export default function More() {
   return (
